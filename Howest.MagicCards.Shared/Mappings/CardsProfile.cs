7fefcb4785ca5292a13b3364103f1f6bb66b0fa1
clint.kindermans <clint.kindermans@student.howest.be>
2024-04-28 10:24:53 +0200

    made the CardReadDTO

---
+++ Howest.MagicCards.Shared/Mappings/CardsProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.Shared.DTO;
+
+namespace Howest.MagicCards.Shared.Mappings
+{
+    public class CardsProfile : Profile
+    {
+        public CardsProfile() 
+        {
+            CreateMap<Card, CardReadDTO>()
+                .ForMember(dto => dto.ImageUrl,
+                            opt => opt.MapFrom(c => c.OriginalImageUrl))
+                .ForMember(dto => dto.Rarity,
+                            opt => opt.MapFrom(c => c.Rarity.Name))
+                .ForMember(dto => dto.Artist,
+                            opt => opt.MapFrom(c => c.Artist.FullName))
+                .ForMember(dto => dto.Set,
+                            opt => opt.MapFrom(c => c.Set.Name));
+        }
+    }
+}
