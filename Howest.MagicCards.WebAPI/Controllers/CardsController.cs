3acf3b62b8be1fed3aff1f1f9a3677fee5a73c5a
clint.kindermans <clint.kindermans@student.howest.be>
2024-04-28 10:22:28 +0200

    made basic controller with GetCards api call

---
+++ Howest.MagicCards.WebAPI/Controllers/CardsController.cs
@@ -0,0 +1,26 @@
+﻿using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.Shared.DTO;
+using Howest.MagicCards.DAL.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Howest.MagicCards.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly ICardRepository _cardRepo;
+
+        public CardsController()
+        {
+            _cardRepo = new CardRepository();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CardReadDTO>> GetCards()
+        {
+            return Ok(_cardRepo.GetAllCards());
+        }
+    }
+}
