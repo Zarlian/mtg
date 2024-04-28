80789251464b8ade98ae87f0a0be267ffc92660e
clint.kindermans <clint.kindermans@student.howest.be>
2024-04-28 10:24:22 +0200

    made basic function to get first 100 cards

---
+++ Howest.MagicCards.DAL/Repositories/CardRepository.cs
@@ -0,0 +1,27 @@
+﻿using Howest.MagicCards.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Repositories
+{
+    public class CardRepository : ICardRepository
+    {
+        private readonly MTGContext _db;
+
+        public CardRepository()
+        {
+            _db = new MTGContext();
+        }
+
+        public IQueryable<Card> GetAllCards()
+        {
+            IQueryable<Card> allCards = _db.Cards.Take(100);
+
+            return allCards;
+        }
+    }
+}
