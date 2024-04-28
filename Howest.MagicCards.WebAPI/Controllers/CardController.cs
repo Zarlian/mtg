5be63e0cc1199e4024d486792b57cc11fdcf524f
clint.kindermans <clint.kindermans@student.howest.be>
2024-04-28 10:25:54 +0200

    Revert "made the CardReadDTO"
    This reverts commit 7fefcb4785ca5292a13b3364103f1f6bb66b0fa1.

---
+++ Howest.MagicCards.WebAPI/Controllers/CardController.cs
@@ -0,0 +1,19 @@
+﻿using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.DAL.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Howest.MagicCards.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController : ControllerBase
+    {
+        private readonly ICardRepository _cardRepo;
+
+        public ActionResult<IEnumerable<Card>> GetCards()
+        {
+            return Ok(_cardRepo.GetAllCards());
+        }
+    }
+}
