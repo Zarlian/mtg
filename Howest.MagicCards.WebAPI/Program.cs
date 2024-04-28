7d3a5bce64dee9cf7b1394600d88d527321fea06
clint.kindermans <clint.kindermans@student.howest.be>
2024-04-28 10:21:50 +0200

    added Dependency's for automapper and for dependency injection for cardrepo

--- Howest.MagicCards.WebAPI/Program.cs
+++ Howest.MagicCards.WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.DAL.Repositories;
+using Type = System.Type;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(new Type[] { typeof(Howest.MagicCards.Shared.Mappings.CardsProfile) });
+
+builder.Services.AddScoped<ICardRepository, CardRepository>();
 
 var app = builder.Build();
 
