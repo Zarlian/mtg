32a1d0ffb42995f0c93555ee03703a0f56541a66
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 15:13:04 +0200

    made Deck model

---
+++ Howest.MagicCards.DAL/Models/Deck.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Howest.MagicCards.DAL.Models
+{
+    public class Deck
+    {
+        [BsonElement("_id")]
+        public int Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("cards")]
+        public List<CardInDeck> Cards { get; set; }
+    }
+}
