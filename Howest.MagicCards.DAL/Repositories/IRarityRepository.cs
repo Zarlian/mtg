5c2825ca98431dacc4846d775a4294100c1d8618
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 15:14:44 +0200

    made rarity repo interface

---
+++ Howest.MagicCards.DAL/Repositories/IRarityRepository.cs
@@ -0,0 +1,14 @@
+﻿using Howest.MagicCards.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Repositories
+{
+    public interface IRarityRepository
+    {
+        public IQueryable<Rarity> GetAllRarities();
+    }
+}
