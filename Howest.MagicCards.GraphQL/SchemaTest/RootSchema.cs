2f47612a9397b252e29919d5c2b2819bd3b60152
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 21:27:57 +0200

    made rootschema

---
+++ Howest.MagicCards.GraphQL/SchemaTest/RootSchema.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+using Howest.MagicCards.GraphQL.Queries;
+
+namespace GraphQLAPI.GraphQLTypes
+{
+    public class RootSchema : Schema
+    {
+        public RootSchema(IServiceProvider provider) : base(provider)
+        {
+            Query = provider.GetRequiredService<RootQuery>();
+        }
+    }
+}
