137486796b01cd88a215bae7812265ac87e36866
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 15:13:18 +0200

    made CardInDeck model

---
+++ Howest.MagicCards.DAL/Models/CardInDeck.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Howest.MagicCards.DAL.Models
+{
+    public class CardInDeck
+    {
+        [BsonElement("_id")]
+        public string Id { get; set; }
+        [BsonElement("count")]
+        public string Count { get; set; }
+    }
+}
