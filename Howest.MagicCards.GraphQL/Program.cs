9421aa2a2eba1a0f9f7df788347927235afb04ea
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 21:31:39 +0200

    updated program to work with graphQL

--- Howest.MagicCards.GraphQL/Program.cs
+++ Howest.MagicCards.GraphQL/Program.cs
@@ -1,4 +1,29 @@
+
+
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
+using GraphQLAPI.GraphQLTypes;
+using Howest.MagicCards.DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager config = builder.Configuration;
+
+builder.Services.AddScoped<ICardRepository, CardRepository>();
+
+builder.Services.AddScoped<RootSchema>();
+builder.Services.AddGraphQL()
+                .AddGraphTypes(typeof(RootSchema), ServiceLifetime.Scoped)
+                .AddDataLoader()
+                .AddSystemTextJson();
+
 var app = builder.Build();
+
+app.UseGraphQL<RootSchema>();
+app.UseGraphQLPlayground(
+    "/ui/playground",
+    new PlaygroundOptions()
+    {
+        EditorTheme = EditorTheme.Dark
+    });
 
 app.Run();
