8dbe520bae9975c060c562360477683ae97b82dc
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 11:27:40 +0200

    fnished GetCards function

--- Howest.MagicCards.WebAPI/Controllers/CardsController.cs
+++ Howest.MagicCards.WebAPI/Controllers/CardsController.cs
@@ -1,14 +1,18 @@
-﻿using Howest.MagicCards.DAL.Models;
-using Howest.MagicCards.Shared.DTO;
+﻿using Howest.MagicCards.Shared.DTO;
 using Howest.MagicCards.DAL.Repositories;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper.QueryableExtensions;
 using AutoMapper;
+using Shared.Wrappers;
+using Shared.Filters;
+using Shared.Extensions;
+using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.Shared.Filters;
 
-namespace Howest.MagicCards.WebAPI.Controllers
+namespace Howest.MagicCards.WebAPI.Controllers.V1_1
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class CardsController : ControllerBase
     {
@@ -22,10 +26,32 @@ namespace Howest.MagicCards.WebAPI.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<CardReadDTO>> GetCards()
+        public ActionResult<PagedResponse<IEnumerable<CardReadDTO>>> GetCards([FromQuery] CardFilter filter)
         {
-            return Ok(_cardRepo.GetAllCards().
-                ProjectTo<CardReadDTO>(_mapper.ConfigurationProvider));
+
+            IQueryable<Card> allCards = _cardRepo.GetAllCards();
+
+            if (allCards == null)
+            {
+                return NotFound("No cards found");
+            }
+
+
+            IQueryable<Card> filteredCards = allCards.ToFilteredList(filter);
+
+            int totalCount = filteredCards.Count();
+            int totalPages = (int)Math.Ceiling(totalCount / (double)filter.PageSize);
+
+            return Ok(new PagedResponse<IEnumerable<CardReadDTO>>(
+                filteredCards
+                .ToPagedList(filter.PageNumber, filter.PageSize)
+                .ProjectTo<CardReadDTO>(_mapper.ConfigurationProvider)
+                .ToList(),
+                filter.PageNumber,
+                filter.PageSize,
+                totalCount,
+                totalPages)                
+                );
         }
     }
 }
