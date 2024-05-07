09a8709580509788ea035da3de642920e975987f
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 15:14:19 +0200

    made rarityRepo to get all rarity

---
+++ Howest.MagicCards.DAL/Repositories/RarityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Howest.MagicCards.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Repositories
+{
+    public class RarityRepository : IRarityRepository
+    {
+        private readonly MTGContext _db;
+
+        public RarityRepository()
+        {
+            _db = new MTGContext();
+        }
+
+        public IQueryable<Rarity> GetAllRarities()
+        {
+            IQueryable<Rarity> allRarities = _db.Rarities.Select(r => r);
+
+            return allRarities;
+        }
+    }
+}
