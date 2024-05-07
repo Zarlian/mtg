3e81553f1b90e34884ff878a98045cc66a999bd1
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 15:13:46 +0200

    made SetRepo to get all sets

---
+++ Howest.MagicCards.DAL/Repositories/SetRepository.cs
@@ -0,0 +1,26 @@
+﻿using Howest.MagicCards.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Repositories
+{
+    public class SetRepository : ISetRepository
+    {
+        private readonly MTGContext _db;
+
+        public SetRepository()
+        {
+            _db = new MTGContext();
+        }
+
+        public IQueryable<Set> GetAllSets()
+        {
+            IQueryable<Set> allSets = _db.Sets.Select(s => s);
+
+            return allSets;
+        }
+    }
+}
