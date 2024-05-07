b1f2d9f6a3d04dc9599801d4b06361eb3125ee1d
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-07 21:32:22 +0200

    made basic function to get cards

---
+++ Howest.MagicCards.GraphQL/Queries/RootQuery.cs
@@ -0,0 +1,32 @@
+﻿using GraphQL.Types;
+using Howest.MagicCards.GraphQL.Types;
+using Howest.MagicCards.DAL.Repositories;
+using GraphQL;
+using Howest.MagicCards.DAL.Models;
+
+namespace Howest.MagicCards.GraphQL.Queries
+{
+    public class RootQuery : ObjectGraphType
+    {
+        public RootQuery(ICardRepository cardRepo) 
+        {
+            Name = "MGT";
+
+             FieldAsync<ListGraphType<Types.CardType>>(
+                name: "cards",
+                arguments: new QueryArguments
+                {
+                    new QueryArgument<StringGraphType> { Name = "power"},
+                    new QueryArgument<StringGraphType> { Name = "toughness"}
+                },
+                resolve: async context =>
+                {
+                    string power = context.GetArgument<string>("power");
+                    string toughness = context.GetArgument<string>("toughness");
+
+                    return await cardRepo.GetAllCardsAsync();
+                }
+                );
+        }
+    }
+}
