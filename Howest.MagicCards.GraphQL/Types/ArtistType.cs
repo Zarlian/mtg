aada543022267290bdbc0456073a35663f194261
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-12 00:07:29 +0200

    added artisttype

---
+++ Howest.MagicCards.GraphQL/Types/ArtistType.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.DAL.Repositories;
+
+namespace Howest.MagicCards.GraphQL.Types
+{
+    public class ArtistType : ObjectGraphType<Artist>
+    {
+        public ArtistType(ICardRepository cardRepo)
+        {
+            Name = "Artist";
+
+            Field(a => a.Id, type: typeof(IdGraphType)).Description("The unique identifier of the artist.");
+            Field(a => a.FullName).Description("The full name of the artist.");
+
+            Field<ListGraphType<CardType>>("All cards made by this artist",
+                               resolve: context => cardRepo.GetCardsByArtistIdAsync(context.Source.Id).ToList());
+        }
+    }
+}
