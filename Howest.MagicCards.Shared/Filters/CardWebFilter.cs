fff0c205678dfb98308cf3d9ed3a0f7acf54e209
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-19 19:08:28 +0200

    changed name of the filter and added validation

---
+++ Howest.MagicCards.Shared/Filters/CardWebFilter.cs
@@ -0,0 +1,22 @@
+﻿
+using Shared.Filters;
+using System.ComponentModel.DataAnnotations;
+
+namespace Howest.MagicCards.Shared.Filters
+{
+    public class CardWebFilter : PaginationFilter
+    {
+        [StringLength(15, ErrorMessage = "Maximum 15 characters allowed for Name.")]
+        public string CardName { get; set; } = string.Empty;
+        [StringLength(50, ErrorMessage = "Maximum 50 characters allowed for Text.")]
+        public string CardText { get; set; } = string.Empty;
+        [StringLength(15, ErrorMessage = "Maximum 15 characters allowed for Card type.")]
+        public string CardType { get; set; } = string.Empty;
+        [StringLength(50, ErrorMessage = "Maximum 50 characters allowed for Set Name.")]
+        public string Set { get; set; } = string.Empty;
+        [StringLength(15, ErrorMessage = "Maximum 15 characters allowed for Artist Name.")]
+        public string Artist { get; set; } = string.Empty;
+        [StringLength(15, ErrorMessage = "Maximum 15 characters allowed for Rarity.")]
+        public string Rarity { get; set; } = string.Empty;
+    }
+}
