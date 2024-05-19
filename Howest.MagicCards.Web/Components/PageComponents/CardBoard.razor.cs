3829b7ae568e7fd7e24bb3ee5cd66368bf59acde
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-19 19:10:16 +0200

    made cardboard to see my cards

---
+++ Howest.MagicCards.Web/Components/PageComponents/CardBoard.razor.cs
@@ -0,0 +1,15 @@
+﻿using Howest.MagicCards.Shared.DTO;
+using Microsoft.AspNetCore.Components;
+
+namespace Howest.MagicCards.Web.Components.PageComponents
+{
+    public partial class CardBoard
+    {
+        [Parameter]
+        public IEnumerable<CardReadDTO> Cards { get; set; }
+
+
+    }
+
+
+}
