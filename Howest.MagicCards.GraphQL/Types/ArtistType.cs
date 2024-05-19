087d6a308a373a4c2d587f6605e515efc1f7560e
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-19 23:05:38 +0200

    fixed issue with getting cards from artist

--- Howest.MagicCards.GraphQL/Types/ArtistType.cs
+++ Howest.MagicCards.GraphQL/Types/ArtistType.cs
@@ -6,15 +6,13 @@ namespace Howest.MagicCards.GraphQL.Types
 {
     public class ArtistType : ObjectGraphType<Artist>
     {
-        public ArtistType(ICardRepository cardRepo)
+        public ArtistType()
         {
             Name = "Artist";
 
             Field(a => a.Id, type: typeof(IdGraphType)).Description("The unique identifier of the artist.");
             Field(a => a.FullName).Description("The full name of the artist.");
-
-            //Field<ListGraphType<CardType>>("All cards made by this artist",
-            //                   resolve: context => cardRepo.GetCardsByArtistIdAsync(context.Source.Id));
+            Field(a => a.Cards, type: typeof(ListGraphType<CardType>)).Description("The cards of the artist.");
         }
     }
 }
