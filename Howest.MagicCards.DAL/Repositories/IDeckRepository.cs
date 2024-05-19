840efc52ca91e899cd5f51b68e269cf45f23fc1e
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-19 19:05:49 +0200

    updated id to string

--- Howest.MagicCards.DAL/Repositories/IDeckRepository.cs
+++ Howest.MagicCards.DAL/Repositories/IDeckRepository.cs
@@ -5,12 +5,12 @@ namespace Howest.MagicCards.DAL.Repositories
     public interface IDeckRepository
     {
         public Task<List<Deck>> GetAllDecksAsync();
-        public Task<Deck> GetDeckAsync(int deckId);
+        public Task<Deck> GetDeckAsync(string deckId);
         public void AddDeck(Deck deck);
         public void UpdateDeckAsync(Deck deck);
-        void UpdateCardInDeckAsync(int deckId, CardInDeck cardInDeck);
+        void UpdateCardInDeckAsync(string deckId, CardInDeck cardInDeck);
         void RemoveAllDecksAsync();
-        public void RemoveDeckAsync(int deckId);        
+        public void RemoveDeckAsync(string deckId);        
         
 
     }
