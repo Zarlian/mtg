747f3073c8ba8771f1e1c2341aed196d65e5f60d
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-19 19:05:26 +0200

    made artist repo for the graphQL

---
+++ Howest.MagicCards.DAL/Repositories/IArtistRepository.cs
@@ -0,0 +1,16 @@
+﻿using Howest.MagicCards.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Repositories
+{
+    public interface IArtistRepository
+    {
+        Task<IQueryable<Artist>> GetAllArtistsAsync();
+
+        Task<Artist> GetArtistByIdAsync(long id);
+    }
+}
