ab19b3a5a2fb56c80b3a80c4952ebd5fd213c41c
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-19 19:07:48 +0200

    changed id to string

--- Howest.MagicCards.Shared/DTO/DeckDTO.cs
+++ Howest.MagicCards.Shared/DTO/DeckDTO.cs
@@ -3,7 +3,7 @@ namespace Howest.MagicCards.Shared.DTO
 {
     public record DeckDTO
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         public string Name { get; set; }
 
