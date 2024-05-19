4e0bf527270fc16e8612ff60f37a217c3db991f7
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-19 19:07:34 +0200

    added name and changed string count to int

--- Howest.MagicCards.Shared/DTO/CardInDeckDTO.cs
+++ Howest.MagicCards.Shared/DTO/CardInDeckDTO.cs
@@ -3,6 +3,7 @@
     public record CardInDeckDTO
     {
         public int Id { get; set; }
-        public string Count { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
     }
 }
